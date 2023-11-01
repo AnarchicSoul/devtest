8876d6e8d88071904bf030a3fe3cb8cf8d6ad1e8
anarchicsoul <duval.johan.91@gmail.com>
2023-11-01 18:49:55 +0100

    deleted:    output/onpremise/main.tf 	deleted:    output/onpremise/pytestapp.tf 	deleted:    output/onpremise/pytestapp.value

--- output/onpremise/pytestapp.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "nginx_ingress" {
-  name       = "pytestapp"
-  repository = "https://anarchicsoul.github.io/python"
-  chart      = "pytestapp"
-  version    = "0.2.0"
-  values = [
-    "${file("pytestapp.value")}"
-  ]
-}
